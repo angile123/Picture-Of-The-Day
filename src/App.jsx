89518e0e451d58391d5625790a0b7660ea5e1290
angile123 <an4007592@gmail.com>
2026-05-07 10:11:19 -0700

    feat : add app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import Form from "./components/Form/Form.jsx";
+import Body from "./components/Body/Body.jsx";
+import { useState } from "react";
 
 function App() {
-  return <p>Hello World!</p>;
+  const [picture, setPicture] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  console.log("re-rendered!");
+
+  return (
+    <>
+      <Form setPicture={setPicture} setLoading={setLoading} setError={setError} />
+      <Body picture={picture} loading={loading} error={error} />
+    </>
+  );
 }
 
 export default App;
