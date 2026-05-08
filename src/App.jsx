b93317d1a713c4fbf7facd1d6eda29183808a354
angile123 <an4007592@gmail.com>
2026-05-07 17:48:08 -0700

    feat : add an effect hook that loads data after initial render

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import Form from "./components/Form/Form.jsx";
 import Body from "./components/Body/Body.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [picture, setPicture] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  console.log("re-rendered!");
+  console.log("re-render");
+
+  useEffect(() => {
+    console.log("effect running");
+    const URL = `https://api.nasa.gov/planetary/apod?api_key=Ij0ZDm6nUW5dhYbMLA6qVoH2hH5zFvDgpyXytaSq&date=2000-04-04`;
+    async function fetchData() {
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) setError("Something went wrong");
+        const result = await response.json();
+        setPicture(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []); // runs on initial mount only.
 
   return (
     <>
