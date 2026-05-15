0942257832c4cbebfd6c5eab384e3fdaa81344fb
angile123 <an4007592@gmail.com>
2026-05-15 11:41:48 -0700

    refactor: change the component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import Body from "./components/Body/Body.jsx";
-
+import { useState } from "react";
+import Grid from "./components/Grid/Grid.jsx";
+import Image from "./components/Image/Image.jsx";
+import Info from "./components/Info/Info.jsx";
+import Form from "./components/Form/Form.jsx";
+import styles from "./App.module.css";
 function App() {
-  return <Body />;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  return (
+    <Grid>
+      <div className={styles.leftDisplay}>
+        <Image loading={loading} data={data} error={error} />
+      </div>
+
+      <div className={styles.rightDisplay}>
+        <div className={styles.rightWrapper}>
+          <Info data={data} loading={loading} error={error} />
+          <Form setData={setData} setLoading={setLoading} setError={setError} />
+        </div>
+      </div>
+    </Grid>
+  );
 }
 
 export default App;
